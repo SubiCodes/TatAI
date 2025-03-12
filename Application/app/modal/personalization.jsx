3c2e22977cfb55806db40c8df66f7728d7c16448
Rein Subisol <subisolreineerkim@gmail.com>
2025-03-12 20:47:50 +0800

    functioning add preferences page

--- Application/app/modal/personalization.jsx
+++ Application/app/modal/personalization.jsx
@@ -1,12 +1,21 @@
-import { View, Text, SafeAreaView, StatusBar, FlatList, Dimensions, StyleSheet, TouchableOpacity, ScrollView, TextInput, Alert } from 'react-native'
+import { View, Text, SafeAreaView, StatusBar, FlatList, Dimensions, StyleSheet, TouchableOpacity, ScrollView, TextInput, Alert, Image, ActivityIndicator } from 'react-native'
 import React, { useCallback, useRef, useState } from 'react'
-import { useFocusEffect, useNavigation } from 'expo-router'
+import { useFocusEffect, useNavigation, useRouter } from 'expo-router'
+import { API_URL } from '@/constants/links.js'
+
+import illustration from '@/assets/images/illustrations/preference_illustration.png'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { jwtDecode } from 'jwt-decode';
+import axios from 'axios'
 
 const {width, height} = Dimensions.get('window');
 
 const Personalization = () => {
 
     const navigation = useNavigation();
+    const router = useRouter();
+    const [userID, setUserID] = useState();
+    const [loading, setLoading] = useState(true);
 
     const removeHeader = () => {
         navigation.setOptions({
@@ -14,9 +23,38 @@ const Personalization = () => {
             headerShown: false, // Ensure the header is visible
         });
     }
+
+    const checkPreferenceExist = async () => {
+        setLoading(true);
+        try {
+            const token = await AsyncStorage.getItem('token');
+            if(!token){
+                await router.replace('/(auth-screens)/signin');
+                return;
+            }
+            const decodedToken = await jwtDecode(token);
+            setUserID(decodedToken.userID);
+            const preference = await axios.get(`${API_URL}/api/v1/preference/${decodedToken.userID}`, 
+                { 
+                  validateStatus: (status) => status < 500,
+                }
+              );
+            if (preference.data.success){
+                await router.replace('/(tabs)/home');
+                return;
+            }
+            setLoading(false);
+        } catch (error) {
+            Alert.alert("Oops⚠️", "Error adding preference to your account");
+            console.log(error.message);
+            setLoading(false);
+        }
+    }
+
     useFocusEffect(
         useCallback(() => {
             removeHeader();
+            checkPreferenceExist();
         }, [])
     )
 
@@ -61,39 +99,239 @@ const Personalization = () => {
         )
     }
 
+    const [preferredToneToBeUsed, setPreferredToneToBeUsed] = useState('');
+
     const preferredTone = () => {
+        const [preferredToneToBeUsedTemp, setPreferredToneToBeUsedTemp] = useState(''); 
+        const updatePreference = () => {
+            if (preferredToneToBeUsedTemp.trim() === ''){
+                Alert.alert("Oops⚠️", "Field cannot be empty. Fill them up before proceeding!", [
+                    {text: 'OK'}
+                ]);
+                return;
+            }
+            setPreferredToneToBeUsed(preferredToneToBeUsedTemp);
+            goNextPage();
+        }
         return(
             <>
-                <Text>Page 2</Text>
+               <ScrollView contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 24, paddingVertical: 20, paddingBottom: 24, gap: 12 }}
+                    style={{ width: width, height: height}}>
+                    <View className='w-[80%] flex-col gap-4'>
+                        <Text className='font-extrabold text-wrap text-4xl'>Personalize your Experience</Text>
+                        <Text className='font-regular text-base'>What kind of tone would you like TatAi to use when speaking?</Text>
+                    </View>
+                    <View className='w-full h-auto gap-2 pt-4'>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setPreferredToneToBeUsedTemp('formal')} style={{backgroundColor: preferredToneToBeUsedTemp === 'formal' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Formal Tone</Text>
+                            {preferredToneToBeUsedTemp === 'formal' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setPreferredToneToBeUsedTemp('casual')} style={{backgroundColor: preferredToneToBeUsedTemp === 'casual' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Casual Tone</Text>
+                            {preferredToneToBeUsedTemp === 'casual' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setPreferredToneToBeUsedTemp('soft spoken')} style={{backgroundColor: preferredToneToBeUsedTemp === 'soft spoken' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Soft Spoken Tone</Text>
+                            {preferredToneToBeUsedTemp === 'soft spoken' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setPreferredToneToBeUsedTemp('strict')} style={{backgroundColor: preferredToneToBeUsedTemp === 'strict' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Strict Tone</Text>
+                            {preferredToneToBeUsedTemp === 'strict' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 1 }} />
+                    <TouchableOpacity className='w-full h-12 rounded items-center justify-center bg-primary' onPress={updatePreference}>
+                        <Text className='font-bold text-xl text-white'>Next</Text>
+                    </TouchableOpacity>
+                </ScrollView>
             </>
         )
     }
 
-    const experienceLevel = () => {
-        return(
-            <>
-                <Text>Page 3</Text>
-            </>
-        )
-    }
+    const [toolKnowledgeLevel, setToolKnowledgeLevel] = useState('');
 
     const toolFamiliarity = () => {
+
+        const [toolKnowledgeLevelTemp, setToolKnowledgeLevelTemp] = useState('');
+        const updatePreference = () => {
+            if (toolKnowledgeLevelTemp.trim() === ''){
+                Alert.alert("Oops⚠️", "Field cannot be empty. Fill them up before proceeding!", [
+                    {text: 'OK'}
+                ]);
+                return;
+            }
+            setToolKnowledgeLevel(toolKnowledgeLevelTemp);
+            goNextPage();
+        }
+
         return(
             <>
-                <Text>Page 4</Text>
+                <ScrollView contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 24, paddingVertical: 20, paddingBottom: 24, gap: 12 }}
+                    style={{ width: width, height: height}}>
+                    <View className='w-[80%] flex-col gap-4'>
+                        <Text className='font-extrabold text-wrap text-4xl'>Personalize your Experience</Text>
+                        <Text className='font-regular text-base'>How would you rate your knowledge and skill in selecting and using various tools?</Text>
+                    </View>
+                    <View className='w-full h-auto gap-2 pt-4'>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setToolKnowledgeLevelTemp('unfamiliar')} style={{backgroundColor: toolKnowledgeLevelTemp === 'unfamiliar' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Unfamiliar</Text>
+                            {toolKnowledgeLevelTemp === 'unfamiliar' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setToolKnowledgeLevelTemp('recognizes basics')} style={{backgroundColor: toolKnowledgeLevelTemp === 'recognizes basics' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Recognizes Basics</Text>
+                            {toolKnowledgeLevelTemp === 'recognizes basics' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setToolKnowledgeLevelTemp('functionally knowledgeable')} style={{backgroundColor: toolKnowledgeLevelTemp === 'functionally knowledgeable' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Functionally Knowledgeable</Text>
+                            {toolKnowledgeLevelTemp === 'functionally knowledgeable' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setToolKnowledgeLevelTemp('knowledgeable')} style={{backgroundColor: toolKnowledgeLevelTemp === 'knowledgeable' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Knowledgeable </Text>
+                            {toolKnowledgeLevelTemp === 'knowledgeable' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setToolKnowledgeLevelTemp('expert')} style={{backgroundColor: toolKnowledgeLevelTemp === 'expert' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Expert</Text>
+                            {toolKnowledgeLevelTemp === 'expert' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 1 }} />
+                    <TouchableOpacity className='w-full h-12 rounded items-center justify-center bg-primary' onPress={updatePreference}>
+                        <Text className='font-bold text-xl text-white'>Next</Text>
+                    </TouchableOpacity>
+                </ScrollView>
             </>
         )
     }
 
-    const interests = () => {
+    const [skillLevel, setSkillLevel] = useState('');
+
+    const skillsLevel = () => {
+
+        const [skillLevelTemp, setSkillLevelTemp] = useState('');
+        const updatePreference = () => {
+            if (skillLevelTemp.trim() === ''){
+                Alert.alert("Oops⚠️", "Field cannot be empty. Fill them up before proceeding!", [
+                    {text: 'OK'}
+                ]);
+                return;
+            }
+            setSkillLevel(skillLevelTemp);
+            goNextPage();
+        }
+
         return(
             <>
-                <Text>Page 5</Text>
+                <ScrollView contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 24, paddingVertical: 20, paddingBottom: 24, gap: 12 }}
+                    style={{ width: width, height: height}}>
+                    <View className='w-[80%] flex-col gap-4'>
+                        <Text className='font-extrabold text-wrap text-4xl'>Personalize your Experience</Text>
+                        <Text className='font-regular text-base'>How would you assess your level of experience or expertise when it comes to home repairs?</Text>
+                    </View>
+                    <View className='w-full h-auto gap-2 pt-4'>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setSkillLevelTemp('beginner')} style={{backgroundColor: skillLevelTemp === 'beginner' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Beginner</Text>
+                            {skillLevelTemp === 'beginner' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setSkillLevelTemp('intermediate')} style={{backgroundColor: skillLevelTemp === 'intermediate' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Intermediate</Text>
+                            {skillLevelTemp === 'intermediate' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setSkillLevelTemp('advance')} style={{backgroundColor: skillLevelTemp === 'advance' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Advance</Text>
+                            {skillLevelTemp === 'advance' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setSkillLevelTemp('expert')} style={{backgroundColor: skillLevelTemp === 'expert' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Expert</Text>
+                            {skillLevelTemp === 'expert' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                        <TouchableOpacity className='w-full h-12 px-8 items-center flex-row bg-btnWhite shadow-lg rounded-lg' 
+                        onPress={() => setSkillLevelTemp('professional')} style={{backgroundColor: skillLevelTemp === 'professional' ? "#e7f6fe" : "#F8F8FF"}}>
+                            <Text className='font-semibold text-lg text-black'>Professional</Text>
+                            {skillLevelTemp === 'professional' ? <Text className='text-lg ml-auto text-secondary'>✔</Text> : null}
+                        </TouchableOpacity>
+                    </View>
+                    <View style={{ flex: 1 }} />
+                    <TouchableOpacity className='w-full h-12 rounded items-center justify-center bg-primary' onPress={updatePreference}>
+                        <Text className='font-bold text-xl text-white'>Next</Text>
+                    </TouchableOpacity>
+                </ScrollView>
             </>
         )
     }
 
-    const pages = [preferredName, preferredTone, experienceLevel, toolFamiliarity, interests]
+    const [buttonLoading, setButtonLoading] = useState(false);
+
+    const submitPreference = async() => {
+        setButtonLoading(true);
+        console.log(`${preferredNameToBeCalled}, ${preferredToneToBeUsed}, ${toolKnowledgeLevel}, ${skillLevel}`);
+        try {
+            const res = await axios.post(`${API_URL}/api/v1/preference`, {userId: userID, preferredName: preferredNameToBeCalled, preferredTone: preferredToneToBeUsed, toolFamiliarity: toolKnowledgeLevel, skillLevel: skillLevel}, 
+                { 
+                  validateStatus: (status) => status < 500, // Only throw errors for 500+ status codes
+                })
+            if (!res.data.success){
+                Alert.alert("Oops⚠️", res.data.message, [
+                    {text: "Okay"}
+                ]);
+                setButtonLoading(false);
+                return;
+            }
+            console.log(res.data.message);
+            Alert.alert("Congratulations🎊", res.data.message, [
+                {text: "Okay"}
+            ]);
+            await router.replace('/(tabs)/home');
+        } catch (error) {
+            Alert.alert("Oops⚠️", error.message, [
+                {text: "Okay"}
+            ]);
+            setButtonLoading(false);
+        }
+
+    }
+    
+
+    const getStarted = () => {
+        return(
+            <>
+                <ScrollView contentContainerStyle={{ flexGrow: 1, paddingHorizontal: 24, paddingVertical: 20, paddingBottom: 24, gap: 12 }}
+                    style={{ width: width, height: height}}>
+                    <View className='w-[80%] flex-col gap-4'>
+                        <Text className='font-extrabold text-wrap text-4xl'>You're All Set!</Text>
+                        <Text className='font-regular text-base'>Click the button to begin your journey in turning your house into a home!</Text>
+                    </View>
+
+                    <View className='w-full h-[50%] items-center justify-center'>
+                    <Image 
+                        source={illustration} 
+                        style={{ width: '100%', height: '100%', marginTop: 42, resizeMode: 'contain' }} 
+                    />
+                    </View>
+
+                    <View className='flex-1'/>
+                   
+                    <TouchableOpacity className='w-full h-12 rounded items-center justify-center bg-primary' onPress={submitPreference} disabled={buttonLoading}>
+                        {buttonLoading ? <ActivityIndicator size={32} color={'white'}/> : <Text className='font-bold text-xl text-white'>Get Started</Text>}
+                    </TouchableOpacity>
+                </ScrollView>
+            </>
+        )
+    }
+
+    const pages = [preferredName, preferredTone, toolFamiliarity, skillsLevel, getStarted]
 
     const Page = ({Component}) => {
         return (
@@ -136,6 +374,16 @@ const Personalization = () => {
             </View>
         )
     } 
+
+    if (loading){
+        return (
+          <View className='w-screen h-screen items-center justify-center gap-4'>
+            <StatusBar translucent={true} backgroundColor={'transparent'}/>
+            <ActivityIndicator size={32} color={'blue'}/>
+            <Text>Loading Assets...</Text>
+          </View>
+        )
+      }
 
 
   return (
--- Backend/controller/preference.controller.js
+++ Backend/controller/preference.controller.js
@@ -1,4 +1,5 @@
 import UserPreference from "../models/preference.model.js";
+import User from "../models/user.model.js";
 import mongoose from "mongoose";
 
 export const getPreference = async (req, res) => {
@@ -21,7 +22,7 @@ export const getPreference = async (req, res) => {
 };
 
 export const createPreference = async (req, res) => {
-    const { userId, preferredName, preferredTone } = req.body;
+    const { userId, preferredName, preferredTone, toolFamiliarity, skillLevel } = req.body;
 
     if (!mongoose.Types.ObjectId.isValid(userId)) {
         return res.status(400).json({ success: false, message: "Invalid user ID format." });
@@ -32,13 +33,25 @@ export const createPreference = async (req, res) => {
         return res.status(400).json({ success: false, message: "User already have an existing preference." });
     }
 
+    const user = await User.findById(userId);
+
     const validTones = ["formal", "casual", "soft spoken", "strict"];
     if (!validTones.includes(preferredTone)) {
         return res.status(400).json({ success: false, message: "Invalid preferred tone." });
     }
 
+    const validToolFamiliarity = ["unfamiliar", "recognizes basics", "functionally knowledgeable", "knowledgeable", "expert"];
+    if (!validToolFamiliarity.includes(toolFamiliarity)) {
+        return res.status(400).json({ success: false, message: "Invalid Tool Familiarity." });
+    }
+
+    const validSkillLevel = ["beginner", "intermediate", "advance", "expert", "professional"];
+    if (!validSkillLevel.includes(skillLevel)) {
+        return res.status(400).json({ success: false, message: "Invalid Skill Level." });
+    }
+
     try {
-        const preference = await UserPreference.create({ userId: userId,preferredName: preferredName,preferredTone: preferredTone });
+        const preference = await UserPreference.create({ userId: userId, email: user.email, preferredName: preferredName,preferredTone: preferredTone, toolFamiliarity: toolFamiliarity, skillLevel: skillLevel });
         res.status(201).json({ success: true, message: "User preference created successfully.", data: preference });
     } catch (error) {
         console.log(error);
@@ -48,7 +61,7 @@ export const createPreference = async (req, res) => {
 
 export const updatePreference = async (req, res) => {
     const { id } = req.params;
-    const { preferredName, preferredTone, previousPrompts } = req.body;
+    const { preferredName, preferredTone, toolFamiliarity, skillLevel, previousPrompts } = req.body;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
         return res.status(400).json({ success: false, message: "Invalid user ID format." });
@@ -57,6 +70,16 @@ export const updatePreference = async (req, res) => {
     const validTones = ["formal", "casual", "soft spoken", "strict"];
     if (preferredTone && !validTones.includes(preferredTone)) {
         return res.status(400).json({ success: false, message: `Invalid preferred tone, choose between: ${validTones}` });
+    }  
+    
+    const validToolFamiliarity = ["unfamiliar", "recognizes basics", "functionally knowledgeable", "knowledgeable", "expert"];
+    if (!validToolFamiliarity.includes(toolFamiliarity)) {
+        return res.status(400).json({ success: false, message: "Invalid Tool Familiarity." });
+    }
+
+    const validSkillLevel = ["beginner", "intermediate", "advance", "expert", "professional"];
+    if (!validSkillLevel.includes(skillLevel)) {
+        return res.status(400).json({ success: false, message: "Invalid Skill Level." });
     }
 
     try {
@@ -67,6 +90,8 @@ export const updatePreference = async (req, res) => {
 
         if (preferredName) preference.preferredName = preferredName;
         if (preferredTone) preference.preferredTone = preferredTone;
+        if (toolFamiliarity) preference.toolFamiliarity = toolFamiliarity;
+        if (skillLevel) preference.skillLevel = skillLevel;
         if (Array.isArray(previousPrompts) && previousPrompts.length > 0) {
             preference.previousPrompts.push(...previousPrompts);
         }
