e14332b7444229ae09be26c17d4f14dfd408b332
Rein Subisol <subisolreineerkim@gmail.com>
2025-04-11 14:15:43 +0800

    Admin login UI

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 
+import Login from './Pages/LoginPages/Login.jsx'
+
 function App() {
 
   return (
     <>
-      <div className='w-screen h-screen flex items-center justify-center'>
-        <h1 className="text-9xl font-bold text-blue-500">Tailwind Testing</h1>
-      </div>
+      <Login/>
     </>
   )
 }
---
+++ Admin/src/Pages/LoginPages/Login.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from 'react'
+
+
+import loginWallpaper from '../../Images/login-wallpaper.jpg'
+import loginLogo from '../../Images/login-logo.png'
+
+function Login() {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [type, setType] = useState('password');
+
+    const showPassword = () => {
+        if(type === 'password'){
+            setType('text')
+        }else{
+            setType('password')
+        }
+    };
+
+    const handleLogin = () => {
+        console.log('Email:', email);
+        console.log('Password:', password);
+    };
+
+  return (
+    <div className='w-screen h-screen flex justify-center items-center flex-row'>
+        <div className='w-3/5 h-full bg-white flex items-center justify-center px-52'>
+            <div className='w-full h-[90%] flex flex-col justify-start items-center px-12 py-8 bg-gray-100 rounded-3xl gap-20'>
+
+                <div className='w-full h-fit flex flex-col justify-center items-center object-contain border-0'>
+                    <img src={loginLogo} className='max-w-[40%]'/>
+                    <h1 className='text-4xl font-bold'>Welcome Admin!</h1>
+                </div>
+
+                <div className='w-full flex flex-col items-center'>
+
+                    <div className='w-full flex flex-col items-center gap-4'>
+                        <input type="text" placeholder='Email' className='w-[70%] h-14 rounded-xl border-2 border-gray-400 px-4 mb-4' onChange={(e) => setEmail(e.target.value)}/>
+                        <input type={type} placeholder='Password' className='w-[70%] h-14 rounded-xl border-2 border-gray-400 px-4 mb-4' onChange={(e) => setPassword(e.target.value)}/>
+                        <div className='w-[70%] h-auto flex flex-row items-center justify-start gap-2'>
+                            <input type="checkbox" className="w-5 h-5" onChange={showPassword} checked={type === 'text'}/>
+                            <span className='text-base'>Show Password</span>
+                            <div className='flex grow'/>
+                            <span className='text-base text-secondary'>Forgot Password</span>
+                        </div>
+                        <button className='w-[70%] h-14 bg-primary text-white rounded-xl cursor-pointer hover:bg-blue-600 mt-4' onClick={handleLogin}>Login</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div className='w-2/5 h-screen bg-white overflow-hidden object-cover'> 
+            <img src={loginWallpaper} className='max-w-[100%] min-h-screen'/>
+        </div>
+    </div>
+  )
+}
+
+export default Login
