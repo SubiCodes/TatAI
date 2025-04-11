5363aac64ea03a6a37053d600e9511509d246d46
Rein Subisol <subisolreineerkim@gmail.com>
2025-04-11 10:17:05 +0800

    React Vite and Tailwind installed to Admin webapp

---
+++ Admin/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-screen h-screen flex items-center justify-center'>
+        <h1 className="text-9xl font-bold text-blue-500">Tailwind Testing</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
