324d985da998cb638bf77f321ac5bfa80edfc7a9
Rein Subisol <subisolreineerkim@gmail.com>
2025-05-21 14:10:10 +0800

    API key change

--- Backend/config/env.js
+++ Backend/config/env.js
@@ -3,4 +3,4 @@ import { config } from "dotenv";
 // Load only the main .env file
 config();
 
-export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET_KEY } = process.env;
+export const { PORT, NODE_ENV, DB_URI, JWT_SECRET, CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_SECRET_KEY, CHATGPT_API } = process.env;
